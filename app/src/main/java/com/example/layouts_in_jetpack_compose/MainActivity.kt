953102501b5b9dc3991ab4f1937e22d2a1e18cce
Minami Munakata <munakata_minami@andfactory.co.jp>
2021-08-10 15:17:20 +0900

    Create firstBaselineToTop

--- app/src/main/java/com/example/layouts_in_jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/example/layouts_in_jetpack_compose/MainActivity.kt
@@ -34,8 +34,12 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.example.layouts_in_jetpack_compose.ui.theme.LayoutsInJetpackComposeTheme
@@ -180,10 +184,46 @@ fun PhotographerCard(modifier: Modifier = Modifier) {
     }
 }
 
-@Preview
+fun Modifier.firstBaselineToTop(
+    firstBaselineToTop: Dp
+) = this.then(
+    layout { measurable, constraints ->
+        val placeable = measurable.measure(constraints)
+
+        // Check the composable has a first baseline
+        check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+        val firstBaseline = placeable[FirstBaseline]
+
+        // Height of the composable with padding - first baseline
+        val placeY = firstBaselineToTop.roundToPx() - firstBaseline
+        val height = placeable.height + placeY
+        layout(placeable.width, height) {
+            // Where the composable gets placed
+            placeable.placeRelative(0, placeY)
+        }
+    }
+)
+
+//@Preview
+//@Composable
+//fun Preview() {
+//    LayoutsInJetpackComposeTheme {
+//        ImageList()
+//    }
+//}
+
+//@Preview(showBackground = true)
+//@Composable
+//fun TextWithPaddingToBaselinePreview() {
+//    LayoutsInJetpackComposeTheme() {
+//        Text("Hi there!", Modifier.padding(top = 32.dp))
+//    }
+//}
+
+@Preview(showBackground = true)
 @Composable
-fun Preview() {
-    LayoutsInJetpackComposeTheme {
-        ImageList()
+fun TextWithPaddingToBaselinePreview() {
+    LayoutsInJetpackComposeTheme() {
+        Text("Hi there!", Modifier.firstBaselineToTop(32.dp))
     }
 }
